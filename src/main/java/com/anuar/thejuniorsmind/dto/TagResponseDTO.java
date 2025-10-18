249a2c16df3f95bd161ace687749756c8f3fc111
AnuarOrozco <anuarorozzco@gmail.com>
2025-10-17 18:30:52 -0600

    refactor(dto): refactor TagResponseDTO since I forgot to add the relation between tag and posts

--- src/main/java/com/anuar/thejuniorsmind/dto/TagResponseDTO.java
+++ src/main/java/com/anuar/thejuniorsmind/dto/TagResponseDTO.java
@@ -1,7 +1,10 @@
 package com.anuar.thejuniorsmind.dto;
 
+import java.util.List;
+
 public record TagResponseDTO(
         Long id,
         String name,
-        String color
+        String color,
+        List<Long> postIds
 ) {}
