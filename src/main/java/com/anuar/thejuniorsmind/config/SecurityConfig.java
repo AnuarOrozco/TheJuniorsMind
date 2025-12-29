99235cfc4465a61d291c74fcfb0055131de04ca6
AnuarOrozco <anuarorozzco@gmail.com>
2025-12-29 17:32:02 -0600

    feat(security): Create UserDetailsServiceImpl class in the service>security package, class implements UserDetailsService and overrides loadUserByUsername method to implement the project logic. Uses UserRepository to retrieve findByEmail method.
    Added authenticationManager method in SecurityConfig that returns an authentication manager.

--- src/main/java/com/anuar/thejuniorsmind/config/SecurityConfig.java
+++ src/main/java/com/anuar/thejuniorsmind/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.anuar.thejuniorsmind.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,5 +29,11 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
