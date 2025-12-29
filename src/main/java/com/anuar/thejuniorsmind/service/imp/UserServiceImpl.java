1e78a8c785c2445ec9ae78858a70985b55fc8515
AnuarOrozco <anuarorozzco@gmail.com>
2025-12-29 17:38:44 -0600

    feat(UserServiceImpl): Injected PasswordEncoder and modified createUser method, we encode the password by saying: user.setPassword(passwordEncoder.encode(user.getPassword()));

--- src/main/java/com/anuar/thejuniorsmind/service/imp/UserServiceImpl.java
+++ src/main/java/com/anuar/thejuniorsmind/service/imp/UserServiceImpl.java
@@ -9,6 +9,7 @@ import com.anuar.thejuniorsmind.repository.UserRepository;
 import com.anuar.thejuniorsmind.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +20,14 @@ import java.util.stream.Collectors;
 @Transactional
 public class UserServiceImpl implements UserService {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     @Override
     public UserResponseDTO createUser(UserRequestDTO userRequest) {
         User user = userMapper.toEntity(userRequest);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = userRepository.save(user);
         return userMapper.toResponseDTO(savedUser);
     }
