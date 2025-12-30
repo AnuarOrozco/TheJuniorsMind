e2c0f4b1b86d89c5632c7ad573ba0b434ad2de58
AnuarOrozco <anuarorozzco@gmail.com>
2025-12-29 18:41:50 -0600

    feat(SecurityConfig): added STATLESS + JWT filter

--- src/main/java/com/anuar/thejuniorsmind/config/SecurityConfig.java
+++ src/main/java/com/anuar/thejuniorsmind/config/SecurityConfig.java
@@ -1,17 +1,22 @@
 package com.anuar.thejuniorsmind.config;
 
+import com.anuar.thejuniorsmind.security.JwtAuthenticationFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,6 +28,9 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable()) // Temporal para pruebas del backend
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.sendError(
@@ -36,7 +44,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/users").permitAll()      // permitir POST register
                         .requestMatchers("/api/users/**").permitAll()   // permitir GETs mientras pruebas
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
