7ce2d5a6dad2ec737f5d48bbd46827b1e80c13b5
AnuarOrozco <anuarorozzco@gmail.com>
2025-12-29 18:43:34 -0600

    fix(SecurityConfig): JwtAuthenticationFilter required initialization so @RequiredArgsConstructor was added

--- src/main/java/com/anuar/thejuniorsmind/config/SecurityConfig.java
+++ src/main/java/com/anuar/thejuniorsmind/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.anuar.thejuniorsmind.config;
 
 import com.anuar.thejuniorsmind.security.JwtAuthenticationFilter;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
