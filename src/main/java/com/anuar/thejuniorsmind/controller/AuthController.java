203987cad43d71cab3c47dedff23703c578a4581
AnuarOrozco <anuarorozzco@gmail.com>
2025-12-29 18:38:21 -0600

    feat(controller): improved login method to respond JWT

--- src/main/java/com/anuar/thejuniorsmind/controller/AuthController.java
+++ src/main/java/com/anuar/thejuniorsmind/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.anuar.thejuniorsmind.controller;
 
 import com.anuar.thejuniorsmind.dto.LoginRequestDTO;
+import com.anuar.thejuniorsmind.service.security.JwtService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO request) {
@@ -28,6 +32,10 @@ public class AuthController {
                 )
         );
 
-        return ResponseEntity.ok().build();
+        String token = jwtService.generateToken(request.email());
+
+        return ResponseEntity.ok(
+                Map.of("token", token)
+        );
     }
 }
